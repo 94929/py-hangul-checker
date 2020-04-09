b73dcb68a00710cf4ea19b927e7501871829aebc
wsy8029 <wsy8029@gmail.com>
2020-04-09 14:44:09 +0900

    Implement Korean spell checker

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    pass
 
+
+
+#sentence = input('입력: ')
+sentence = '부담갖지말고먹어'
+url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=jQuery112407704652679626749_1586403615462&q=' + sentence + '&where=nexearch&color_blindness=0&_=1586403615463'
+html = requests.get(url).text
+
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.find('em').get_text())
