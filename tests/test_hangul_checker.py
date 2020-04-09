b810154f0474b5bbbe4beab0264064ccdf63b692
Jinsung Ha <jsung5381@naver.com>
2020-04-09 19:02:38 +0900

    Add unittest for check spell method

---
+++ tests/test_hangul_checker.py
@@ -0,0 +1,20 @@
+import unittest
+
+from unittest import mock
+
+from hangul_checker.hangul_checker import KoreanSpellChecker
+
+
+class TestKoreanSpellChecker(unittest.TestCase):
+
+    def setUp(self):
+        self.korean_spell_checker = KoreanSpellChecker()
+
+    def tearDown(self):
+        del self.korean_spell_checker
+
+    def test_check_spelling(self):
+        sentence_in = u'부담갖지 말고 드세요!'
+        sentence_out = self.korean_spell_checker.check_spelling(sentence_in)
+        self.assertEqual(sentence_out, u'부담 갖지 말고 드세요!')
+
