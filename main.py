60047ef4ac1b31cf3827bd457a8d5ac04473953e
Jinsung Ha <jsung5381@naver.com>
2020-04-09 15:02:47 +0900

    Refactor spell-checker

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import requests
+
 from bs4 import BeautifulSoup
 
 
+class KoreanSpellChecker:
+    spell_checker_url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=jQuery112407704652679626749_1586403615462&q={}&where=nexearch&color_blindness=0&_=1586403615463'
+
+    def __check_sentence_len(self, sentence):
+        if len(sentence) > 500:
+            raise Exception("Input sentence length exceeds maximum limit")
+
+    def check_spelling(self, sentence):
+        self.__check_sentence_len(sentence)
+
+        dest_path = self.spell_checker_url.format(sentence)
+        html = requests.get(dest_path).text
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup.find('em').get_text()
 
 
-#sentence = input('입력: ')
-sentence = '부담갖지말고먹어'
-url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=jQuery112407704652679626749_1586403615462&q=' + sentence + '&where=nexearch&color_blindness=0&_=1586403615463'
-html = requests.get(url).text
+if __name__ == '__main__':
+    sentence = "부담갖지말고먹어"
+    ksc = KoreanSpellChecker()
+    res = ksc.check_spelling(sentence)
+    print(res)
 
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.find('em').get_text())
